4642e7b3aa71cf16ba3f482eea83954651353181
RubenKingCP <kostaslaiosedu@gmail.com>
2024-03-30 14:08:35 +0200

    Add a textfield and show text in it when button is pressed

--- Window.java
+++ Window.java
@@ -7,34 +7,47 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Window {
     private static JFrame frame;
-    private static JPanel panel;
+    private static JPanel panel1;
+    private static JPanel panel2;
     private static JButton button;
-
+    private static TextField textField;
 
     public static void main(String[] args) {
         frame = new JFrame();
-        panel = new JPanel();
+        panel1 = new JPanel();
+        panel2 = new JPanel();
         button = new JButton();
+        textField = new TextField();
 
         //Adding an action listner to the button
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                JOptionPane.showMessageDialog(panel, "clicked");
+                JOptionPane.showMessageDialog(panel1, textField.getText());
             }
         });
+        
+        //CUstomize textfield
+        textField.setPreferredSize(new Dimension(200, 50));
 
-        //Add button to the jpanel
-        panel.add(button);
+        //Add button to the jpanel1
+        panel1.add(button);
 
-        // Set preferred size and background color for the panel
-        panel.setPreferredSize(new Dimension(200, 200));
-        panel.setBackground(Color.RED);
+        //Add textfield to panel2
+        panel2.add(textField);
+
+        // Set preferred size and background color for the panels
+        panel1.setPreferredSize(new Dimension(200, 200));
+        panel1.setBackground(Color.RED);
+
+        panel2.setPreferredSize(new Dimension(200, 200));
+        panel2.setBackground(Color.PINK);
 
         // Set layout manager for the content pane
         frame.getContentPane().setLayout(new BorderLayout());
@@ -42,8 +55,9 @@ public class Window {
         // Set background color for the content pane of the frame
         frame.getContentPane().setBackground(Color.BLUE);
 
-        // Add panel to frame
-        frame.getContentPane().add(panel, BorderLayout.SOUTH);
+        // Add panels to frame
+        frame.getContentPane().add(panel1, BorderLayout.SOUTH);
+        frame.getContentPane().add(panel2, BorderLayout.NORTH);
 
         // Set frame properties
         frame.setSize(new Dimension(400, 400));
