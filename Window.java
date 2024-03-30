3b4ab2d85c77b38f69902b816462e77a0845ddab
RubenKingCP <kostaslaiosedu@gmail.com>
2024-03-30 11:42:34 +0200

    Add a jpanel with red color

--- Window.java
+++ Window.java
@@ -1,15 +1,35 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
-
 public class Window {
     private static JFrame frame;
+    private static JPanel panel;
+
+
     public static void main(String[] args) {
         frame = new JFrame();
-        frame.setSize(new Dimension(400, 400));
+        panel = new JPanel();
+
+        // Set preferred size and background color for the panel
+        panel.setPreferredSize(new Dimension(200, 200));
+        panel.setBackground(Color.RED);
+
+        // Set layout manager for the content pane
+        frame.getContentPane().setLayout(new BorderLayout());
+        
+        // Set background color for the content pane of the frame
         frame.getContentPane().setBackground(Color.BLUE);
+
+        // Add panel to frame
+        frame.getContentPane().add(panel, BorderLayout.SOUTH);
+
+        // Set frame properties
+        frame.setSize(new Dimension(400, 400));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
